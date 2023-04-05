2dc853f1af4527337fede2b9ea6fd55dac6e328e
dogukanincee <32237602+dogukanincee@users.noreply.github.com>
2023-04-05 09:08:44 +0200

    Implementations of Reminder

--- app/src/main/java/com/dogukanincee/reminder/Reminder.kt
+++ app/src/main/java/com/dogukanincee/reminder/Reminder.kt
@@ -1,6 +1,11 @@
 package com.dogukanincee.reminder
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class Reminder(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val description: String,
